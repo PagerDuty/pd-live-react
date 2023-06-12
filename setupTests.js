3630bf208f2f1d909423a0e6adf86b05c22b3778
Gavin Reynolds <greynolds@pagerduty.com>
2023-06-12 14:05:41 +0100

    Mock React Resize Detector for testing in Enzyme and Jest
    Signed-off-by: Gavin Reynolds <greynolds@pagerduty.com>

--- setupTests.js
+++ setupTests.js
@@ -8,3 +8,20 @@ import replaceAllInserter from 'string.prototype.replaceall';
 
 configure({ adapter: new Adapter() });
 replaceAllInserter.shim();
+
+// https://github.com/maslianok/react-resize-detector#testing-with-enzyme-and-jest
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn()
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
