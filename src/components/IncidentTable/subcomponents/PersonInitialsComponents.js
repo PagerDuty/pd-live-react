2dfaf544031445d3f0cdb09eeb3953db933b6ea5
Martin Stone <martindstone@me.com>
2023-07-14 11:04:45 -0400

    nicer avatargroup for the personinitialscomponents

--- src/components/IncidentTable/subcomponents/PersonInitialsComponents.js
+++ src/components/IncidentTable/subcomponents/PersonInitialsComponents.js
@@ -3,12 +3,21 @@ import {
 } from 'react-redux';
 
 import {
-  Avatar, AvatarGroup, Tooltip, Link,
+  Avatar,
+  AvatarGroup,
+  Link,
+  Popover,
+  PopoverArrow,
+  PopoverTrigger,
+  PopoverContent,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Th,
+  Tr,
+  Td,
 } from '@chakra-ui/react';
-
-// import {
-//   getInitials,
-// } from 'util/helpers';
 
 const PersonInitialsComponent = ({
   users, displayedUsers,
@@ -31,37 +40,74 @@ const PersonInitialsComponent = ({
         // initials: getInitials(user.summary),
         id: user.id,
         html_url: user.html_url,
+        email: usersMap[user.id].email,
         color: CSS.supports && CSS.supports('color', color) ? color : 'black',
       };
     })
     : [];
 
+  const popoverContent = (
+    <TableContainer>
+      <Table size="sm">
+        <Thead>
+          <Tr>
+            <Th>Summary</Th>
+            <Th>Email</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {displayedUsersByInitials.map((user) => (
+            <Tr key={user.id}>
+              <Td>
+                <Link key={user.id} isExternal href={user.html_url}>
+                  {user.summary}
+                </Link>
+              </Td>
+              <Td>
+                <Link key={user.id} isExternal href={`mailto:${user.email}`}>
+                  {user.email}
+                </Link>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
   return (
     <div>
-      <AvatarGroup size="sm" max={3}>
-        {displayedUsersByInitials.map((user) => (
-          <Tooltip key={user.id} label={user.summary} aria-label={user.summary}>
-            <Link isExternal href={user.html_url}>
-              <Avatar
-                color="white"
-                mr={1}
-                whiteSpace="nowrap"
-                overflow="hidden"
-                name={user.summary}
-                href={user.html_url}
-                size="sm"
-                bg={user.color}
-                getInitials={(name) => {
-                  const allNames = name.trim().split(' ');
-                  const firstInitial = allNames[0].match(/./u);
-                  const lastInitial = allNames[allNames.length - 1].match(/./u);
-                  return `${firstInitial}${lastInitial}`;
-                }}
-              />
-            </Link>
-          </Tooltip>
-        ))}
-      </AvatarGroup>
+      <Popover trigger="hover" size="content">
+        <PopoverTrigger>
+          <AvatarGroup size="sm" max={3} spacing="-0.6rem">
+            {displayedUsersByInitials.map((user) => (
+              // <Tooltip key={user.id} label={user.summary} aria-label={user.summary}>
+              <Link isExternal href={user.html_url}>
+                <Avatar
+                  showBorder
+                  color="white"
+                  mr={1}
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  name={user.summary}
+                  href={user.html_url}
+                  size="sm"
+                  bg={user.color}
+                  getInitials={(name) => {
+                    const allNames = name.trim().split(' ');
+                    const firstInitial = allNames[0].match(/./u);
+                    const lastInitial = allNames[allNames.length - 1].match(/./u);
+                    return `${firstInitial}${lastInitial}`;
+                  }}
+                />
+              </Link>
+            ))}
+          </AvatarGroup>
+        </PopoverTrigger>
+        <PopoverContent w="content">
+          <PopoverArrow />
+          {popoverContent}
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
