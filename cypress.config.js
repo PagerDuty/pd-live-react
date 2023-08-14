8067d5e2929e0d9573adf30f2d665a5be00713f1
Gavin Reynolds <greynolds@pagerduty.com>
2023-08-14 17:17:35 +0100

    fix process.env for cypress
    Signed-off-by: Gavin Reynolds <greynolds@pagerduty.com>

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       dotenv.config();
       cypressFailFast(on, config);
-      config.env.PD_USER_TOKEN = import.meta.env.VITE_PD_USER_TOKEN;
+      config.env.PD_USER_TOKEN = process.env.VITE_PD_USER_TOKEN;
       return config;
     },
     baseUrl: "http://localhost:3000/pd-live-react",
