5e2993582538a9b8cf89f3b299f926ee1fc25a14
Gavin Reynolds <greynolds@pagerduty.com>
2023-08-16 16:10:13 +0100

    Update columns modal component test
    Signed-off-by: Gavin Reynolds <greynolds@pagerduty.com>

--- src/components/ColumnsModal/ColumnsModalComponent.test.js
+++ src/components/ColumnsModal/ColumnsModalComponent.test.js
@@ -8,10 +8,14 @@ import {
   HTML5Backend,
 } from 'react-dnd-html5-backend';
 import {
-  mockStore, componentWrapper,
+  componentWrapper, screen,
+} from 'src/custom-testing-lib';
+import {
+  mockStore,
 } from 'mocks/store.test';
 
 import ColumnsModalComponent from './ColumnsModalComponent';
+import { within } from 'custom-testing-lib';
 
 describe('ColumnsModalComponent', () => {
   let baseStore;
@@ -53,11 +57,11 @@ describe('ColumnsModalComponent', () => {
         incidentTableColumns: [
           { Header: '#', accessorPath: null, columnType: 'incident' },
           { Header: 'Status', accessorPath: null, columnType: 'incident' },
-          { Header: 'CustomField', accessorPath: 'details.to.some.path', columnType: 'incident' },
+          { Header: 'CustomField', accessorPath: 'details.to.some.path', columnType: 'alert' },
           {
             Header: 'Fav Flavour',
             accessorPath: "details.['favorite ice cream flavor']",
-            columnType: 'incident',
+            columnType: 'alert',
           },
         ],
       },
@@ -67,63 +71,57 @@ describe('ColumnsModalComponent', () => {
         <ColumnsModalComponent />
       </DndProvider>
     );
+    store = mockStore(baseStore);
+    componentWrapper(store, DndColumnsModalComponent);
   });
 
   it('should render modal', () => {
-    store = mockStore(baseStore);
-
-    const wrapper = componentWrapper(store, DndColumnsModalComponent);
-    expect(wrapper.find('.chakra-modal__header').contains('Incident Table')).toBeTruthy();
-  });
-
-  // FIXME: Column drag & drop
-
-  it('should display incident table settings', () => {
-    store = mockStore(baseStore);
-    const wrapper = componentWrapper(store, DndColumnsModalComponent);
-
-    expect(wrapper.find('h2').contains('Available')).toBeTruthy();
-    expect(wrapper.find('#incident-column-select')).toBeTruthy();
-    expect(wrapper.find('h2').contains('Custom')).toBeTruthy();
-    expect(wrapper.find('#alert-column-definition-select')).toBeTruthy();
-    expect(wrapper.find('#save-columns-button').contains('OK')).toBeTruthy();
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toHaveTextContent('Incident Table');
+    expect(screen.getByRole('heading', { name: 'Selected' })).toBeInTheDocument();
+    // 2 standard fields, 3 custom fields, 2 added custom fields
+    expect(screen.getAllByLabelText('Remove column')).toHaveLength(7);
+    expect(screen.getByRole('heading', { name: 'Available' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Custom' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'OK' })).toBeInTheDocument();
   });
 
   it('should render an enabled custom column option with unique header name', () => {
-    store = mockStore(baseStore);
-    const wrapper = componentWrapper(store, DndColumnsModalComponent);
-    const customColumns = wrapper.find('div#custom-columns-card-body');
-    const selectedColumns = wrapper.find('div#selected-columns-card-body');
-    const availableColumns = wrapper.find('div#available-columns-card-body');
-    expect(customColumns.find('span[value="CustomField:details.to.some.path"]')).toBeTruthy();
-    expect(selectedColumns.find('span[value="CustomField"]')).toBeTruthy();
-    expect(availableColumns.find('span[value="CustomField"]')).toEqual({});
+    // Find the Custom header, and find the CustomField DnD box near it
+    const customColumns = screen.getByText('Custom').parentElement.parentElement;
+    const customColumn = within(customColumns).getByText('CustomField:details.to.some.path');
+    expect(customColumn).toBeInTheDocument();
+    expect(within(customColumn).getByLabelText('Remove column')).toBeInTheDocument();
+
+    // Find the Selected header, and find the Selected DnD box near it
+    const selectedColumns = screen.getByText('Selected').parentElement.parentElement;
+    const selectedColumn = within(selectedColumns).getByText('CustomField');
+    expect(selectedColumn).toBeInTheDocument();
+    expect(within(selectedColumn).getByLabelText('Remove column')).toBeInTheDocument();
   });
 
   it('should render an available custom column option with unique header name', () => {
-    store = mockStore(baseStore);
-    const wrapper = componentWrapper(store, DndColumnsModalComponent);
-    const customColumns = wrapper.find('div#custom-columns-card-body');
-    const selectedColumns = wrapper.find('div#selected-columns-card-body');
-    const availableColumns = wrapper.find('div#available-columns-card-body');
-    expect(
-      customColumns.find('span[value="AnotherCustomField:details.to.some.other.path"]'),
-    ).toBeTruthy();
-    expect(selectedColumns.find('span[value="AnotherCustomField"]')).toEqual({});
-    expect(availableColumns.find('span[value="AnotherCustomField"]')).toBeTruthy();
+    const customColumns = screen.getByText('Custom').parentElement.parentElement;
+    const customColumn = within(customColumns).getByText('AnotherCustomField:details.to.some.other.path');
+    expect(customColumn).toBeInTheDocument();
+    expect(within(customColumn).getByLabelText('Remove column')).toBeInTheDocument();
+
+    const availableColumns = screen.getByText('Available').parentElement.parentElement;
+    const availableColumn = within(availableColumns).getByText('AnotherCustomField');
+    expect(availableColumn).toBeInTheDocument();
+    expect(within(availableColumn).getByLabelText('Add column')).toBeInTheDocument();
   });
 
   it('should render an enabled custom column option with JSON path containing spaces', () => {
-    store = mockStore(baseStore);
-    const wrapper = componentWrapper(store, DndColumnsModalComponent);
-    const customColumns = wrapper.find('div#custom-columns-card-body');
-    const selectedColumns = wrapper.find('div#selected-columns-card-body');
-    const availableColumns = wrapper.find('div#available-columns-card-body');
-    expect(
-      customColumns.find('span[value="Fav Flavour:details.[\'favorite ice cream flavor\']"]'),
-    ).toBeTruthy();
-    expect(selectedColumns.find('span[value="Fav Flavour"]')).toEqual({});
-    expect(availableColumns.find('span[value="Fav Flavour"]')).toBeTruthy();
+    const customColumns = screen.getByText('Custom').parentElement.parentElement;
+    const customColumn = within(customColumns).getByText('Fav Flavour:details.[\'favorite ice cream flavor\']');
+    expect(customColumn).toBeInTheDocument();
+    expect(within(customColumn).getByLabelText('Remove column')).toBeInTheDocument();
+
+    const selectedColumns = screen.getByText('Selected').parentElement.parentElement;
+    const selectedColumn = within(selectedColumns).getByText('Fav Flavour');
+    expect(selectedColumn).toBeInTheDocument();
+    expect(within(selectedColumn).getByLabelText('Remove column')).toBeInTheDocument();
   });
 
   // it('should render a disabled custom column option which has a duplicate header/name', () => {
