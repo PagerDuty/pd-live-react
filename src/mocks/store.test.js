3a1b2ebf0be6dd31cd1cee811f8a5175c2352b93
Gavin Reynolds <greynolds@pagerduty.com>
2023-08-17 10:51:59 +0100

    Remove redundant comment [skip ci]
    Signed-off-by: Gavin Reynolds <greynolds@pagerduty.com>

--- src/mocks/store.test.js
+++ src/mocks/store.test.js
@@ -2,5 +2,3 @@ import configureStore from 'redux-mock-store';
 
 export const mockStore = configureStore([]);
 export default mockStore;
-
-// Ref: https://stackoverflow.com/a/59864054/6480733 - required for test mocking
